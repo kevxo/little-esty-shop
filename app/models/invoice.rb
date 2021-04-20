9a495f5eb0f931cec8bb3ff6b37e5e23f8a11f74
Kevin Cuadros <kev040798@gmail.com>
2021-04-20 16:13:25 -0400

    Set up relationships and validations for models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,7 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
+
+  has_many :transactions
+  has_many :invoice_items, dependent: :destroy
+  has_many :items, through: :invoice_items
 end
