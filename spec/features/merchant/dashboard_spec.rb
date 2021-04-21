10a56f294c78d8272a74220b0da237d0ebbc8daa
Kevin Cuadros <kev040798@gmail.com>
2021-04-21 18:21:53 -0400

    Started on test for top 5 customers for view

--- spec/features/merchant/dashboard_spec.rb
+++ spec/features/merchant/dashboard_spec.rb
@@ -3,6 +3,48 @@ require 'rails_helper'
 RSpec.describe 'Merchant Dashboard' do
   describe 'As a merchant' do
     describe 'When I visit my merchant dashboard' do
+      before(:each) do
+        @merchant = create(:merchant)
+
+        @customer1 = create(:customer)
+        @customer2 = create(:customer)
+        @customer3 = create(:customer)
+        @customer4 = create(:customer)
+        @customer5 = create(:customer)
+
+        @item1 = create(:item, merchant_id: @merchant.id)
+        @item2 = create(:item, merchant_id: @merchant.id)
+        @item3 = create(:item, merchant_id: @merchant.id)
+        @item4 = create(:item, merchant_id: @merchant.id)
+
+        @invoice1 = create(:invoice, customer_id: @customer1.id)
+        @invoice2 = create(:invoice, customer_id: @customer1.id)
+        @invoice3 = create(:invoice, customer_id: @customer2.id)
+        @invoice4 = create(:invoice, customer_id: @customer2.id)
+        @invoice5 = create(:invoice, customer_id: @customer2.id)
+        @invoice6 = create(:invoice, customer_id: @customer3.id)
+        @invoice7 = create(:invoice, customer_id: @customer4.id)
+        @invoice8 = create(:invoice, customer_id: @customer5.id)
+
+        create(:transaction, invoice_id: @invoice1.id)
+        create(:transaction, invoice_id: @invoice2.id)
+        create(:transaction, invoice_id: @invoice3.id)
+        create(:transaction, invoice_id: @invoice4.id)
+        create(:transaction, invoice_id: @invoice5.id)
+        create(:transaction, invoice_id: @invoice6.id)
+        create(:transaction, invoice_id: @invoice7.id)
+        create(:transaction, invoice_id: @invoice8.id)
+
+        create(:invoice_item, invoice_id: @invoice1.id, item_id: @item1.id)
+        create(:invoice_item, invoice_id: @invoice2.id, item_id: @item3.id)
+        create(:invoice_item, invoice_id: @invoice3.id, item_id: @item4.id)
+        create(:invoice_item, invoice_id: @invoice4.id, item_id: @item1.id)
+        create(:invoice_item, invoice_id: @invoice7.id, item_id: @item2.id)
+        create(:invoice_item, invoice_id: @invoice8.id, item_id: @item3.id)
+        create(:invoice_item, invoice_id: @invoice6.id, item_id: @item3.id)
+        create(:invoice_item, invoice_id: @invoice5.id, item_id: @item4.id)
+      end
+
       it 'should see the name of my merchant' do
         merchant = create(:merchant)
 
@@ -19,6 +61,14 @@ RSpec.describe 'Merchant Dashboard' do
         within '.links' do
           expect(page).to have_link('Items')
           expect(page).to have_link('Invoices')
+        end
+      end
+
+      it 'should see top 5 customers with successful transactions with merchant ' do
+        visit "/merchant/#{@merchant.id}/dashboard"
+
+        within '.top-5-customers' do
+          expect(page).to have_content('Top 5 Customers')
         end
       end
     end
