35db0ae6b8b4bfb5501fc4b8f3f49fb4a5130c3a
Kevin Cuadros <kev040798@gmail.com>
2021-04-23 12:55:19 -0400

    Working on top 5 customer with most transactions with merchant.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,8 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
+
+  def self.top_5
+    require 'pry' ; binding.pry
+  end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -7,4 +7,54 @@ RSpec.describe Invoice, type: :model do
     it { should have_many(:invoice_items) }
     it { should have_many(:items).through(:invoice_items) }
   end
+
+  describe 'class method methods' do
+    before(:each) do
+      @merchant = create(:merchant)
+
+      @customer1 = create(:customer)
+      @customer2 = create(:customer)
+      @customer3 = create(:customer)
+      @customer4 = create(:customer)
+      @customer5 = create(:customer)
+
+      @item1 = create(:item, merchant_id: @merchant.id)
+      @item2 = create(:item, merchant_id: @merchant.id)
+      @item3 = create(:item, merchant_id: @merchant.id)
+      @item4 = create(:item, merchant_id: @merchant.id)
+
+      @invoice1 = create(:invoice, customer_id: @customer1.id)
+      @invoice2 = create(:invoice, customer_id: @customer1.id)
+      @invoice3 = create(:invoice, customer_id: @customer2.id)
+      @invoice4 = create(:invoice, customer_id: @customer2.id)
+      @invoice5 = create(:invoice, customer_id: @customer2.id)
+      @invoice6 = create(:invoice, customer_id: @customer3.id)
+      @invoice7 = create(:invoice, customer_id: @customer4.id)
+      @invoice8 = create(:invoice, customer_id: @customer5.id)
+
+      create(:transaction, invoice_id: @invoice1.id)
+      create(:transaction, invoice_id: @invoice2.id)
+      create(:transaction, invoice_id: @invoice3.id)
+      create(:transaction, invoice_id: @invoice4.id)
+      create(:transaction, invoice_id: @invoice5.id)
+      create(:transaction, invoice_id: @invoice6.id)
+      create(:transaction, invoice_id: @invoice7.id)
+      create(:transaction, invoice_id: @invoice8.id)
+
+      create(:invoice_item, invoice_id: @invoice1.id, item_id: @item1.id)
+      create(:invoice_item, invoice_id: @invoice2.id, item_id: @item3.id)
+      create(:invoice_item, invoice_id: @invoice3.id, item_id: @item4.id)
+      create(:invoice_item, invoice_id: @invoice4.id, item_id: @item1.id)
+      create(:invoice_item, invoice_id: @invoice7.id, item_id: @item2.id)
+      create(:invoice_item, invoice_id: @invoice8.id, item_id: @item3.id)
+      create(:invoice_item, invoice_id: @invoice6.id, item_id: @item3.id)
+      create(:invoice_item, invoice_id: @invoice5.id, item_id: @item4.id)
+    end
+
+    it 'top 5 customers with successful transactions' do
+      expected = [@customer2, @customer1, @customer3, @customer4, @customer5]
+
+      expect(Invoice.top_5).to eq(expected)
+    end
+  end
 end
