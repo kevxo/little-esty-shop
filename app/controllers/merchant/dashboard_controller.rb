58248c84d793c5a84c0378b867008412df49990c
Kevin Cuadros <kev040798@gmail.com>
2021-04-23 16:00:00 -0400

    Display top 5 customers that have successful transactions with merchant

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Merchant::DashboardController < ApplicationController
   def index
     @merchant = Merchant.find(params[:id])
+    @top_successful_transactions = Invoice.most_successful_transactions(@merchant.id)
   end
 end
