6820ca4544da883ee462dc4007606d7545220639
Kevin Cuadros <kev040798@gmail.com>
2021-04-23 20:16:50 -0400

    Create instance method for items that are ready to ship

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
+
+  def ready_to_ship
+    invoice_items.where(status: 'packaged').order(created_at: 'DESC')
+  end
 end
