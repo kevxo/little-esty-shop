fcbbbb7c786382c1634425b3ac88b3f813de7bd6
Kevin Cuadros <kev040798@gmail.com>
2021-04-25 22:34:56 -0400

    Create test for merchant item show page

--- spec/features/merchant/items_spec.rb
+++ spec/features/merchant/items_spec.rb
@@ -35,4 +35,28 @@ RSpec.describe 'As a merchant' do
       end
     end
   end
+
+  describe 'When I click on an item' do
+    it 'should take me to the item show page and the items attributes' do
+      merchant = create(:merchant)
+
+      item1 = create(:item, merchant_id: merchant.id)
+
+      visit "/merchant/#{merchant.id}/items"
+
+      within '.my-items' do
+        expect(page).to have_link(item1.name)
+
+        click_link item1.name
+      end
+
+      expect(current_path).to eq("/merchant/#{merchant.id}/items/#{item1.id}")
+
+      within '.item-info' do
+        expect(page).to have_content("Name: #{item1.name}")
+        expect(page).to have_content("Description: #{item1.description}")
+        expect(page).to have_content("Current Selling Price: $#{item1.unit_price}")
+      end
+    end
+  end
 end
