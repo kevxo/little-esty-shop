fef695ce7e456af730638f0f3a98ff14e68e6dce
Kevin Cuadros <kev040798@gmail.com>
2021-04-25 22:34:20 -0400

    Create route for merchant item show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     get '/:id/dashboard', to: 'dashboard#index'
 
     get '/:id/items', to: 'items#index'
+    get '/:id/items/:item_id', to: 'items#show'
   end
 end
