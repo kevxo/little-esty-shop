332cb8acbd2cc812f27158e59b5461d55888840f
Kevin Cuadros <kev040798@gmail.com>
2021-04-26 18:17:30 -0400

    Create the edit action and the update action for an item

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -4,6 +4,27 @@ class Merchant::ItemsController < ApplicationController
   end
 
   def show
+    @merchant = Merchant.find(params[:id])
     @item = Item.find(params[:item_id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+    @item = Item.find(params[:item_id])
+  end
+
+  def update
+    item = Item.find(params[:item_id])
+    merchant = Merchant.find(params[:id])
+    item.update(item_params)
+
+    flash[:notice] = 'Item has been updated successfully'
+    redirect_to "/merchant/#{merchant.id}/items/#{item.id}"
+  end
+
+  private
+
+    def item_params
+      params.permit(:name, :description, :unit_price)
+    end
 end
