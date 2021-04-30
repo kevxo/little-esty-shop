e5a429ab6a88c1c4c79afcea6eea1d3ff37fa7c5
Kevin Cuadros <kev040798@gmail.com>
2021-04-30 18:17:51 -0400

    Create Top 5 popular items class method

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,14 @@ class Item < ApplicationRecord
   def ready_to_ship
     invoice_items.where(status: 'packaged').order(created_at: 'DESC')
   end
+
+  def self.most_popular_items(limit = 5, order = 'DESC', merchant_id)
+    select('items.name, items.id, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+      .joins(:invoice_items, invoices: :transactions)
+      .where(transactions: { result: 0 }, merchant_id: merchant_id)
+      .distinct
+      .group(:id)
+      .order("revenue #{order}")
+      .limit(limit)
+  end
 end
