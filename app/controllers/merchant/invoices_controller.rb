1a54fa2df41766572e079b017e65d3d58e62c629
Kevin Cuadros <kev040798@gmail.com>
2021-05-02 21:24:54 -0400

    Create invoice controller for merchant with index and show

---
+++ app/controllers/merchant/invoices_controller.rb
@@ -0,0 +1,10 @@
+class Merchant::InvoicesController < ApplicationController
+  def index
+    @merchant = Merchant.find(params[:id])
+    invoice_ids = InvoiceItem.where(item_id: @merchant.items.pluck(:id)).pluck(:invoice_id)
+    @invoices = Invoice.where(id: invoice_ids)
+  end
+
+  def show
+  end
+end
