6961e93508faaafa1d9fc8a9009fb31dffef62e2
Kevin Cuadros <kev040798@gmail.com>
2021-05-03 15:41:03 -0400

    Test for invoice show page

--- spec/features/merchant/invoice_spec.rb
+++ spec/features/merchant/invoice_spec.rb
@@ -66,4 +66,60 @@ RSpec.describe 'As a Merchant' do
       expect(current_path).to eq("/merchant/#{@merchant.id}/invoices/#{@invoice1.id}")
     end
   end
+
+  describe 'When I visit the invoice show page' do
+    before(:each) do
+      @merchant = create(:merchant)
+
+      @customer1 = create(:customer)
+      @customer2 = create(:customer)
+      @customer3 = create(:customer)
+      @customer4 = create(:customer)
+      @customer5 = create(:customer)
+
+      @item1 = create(:item, merchant_id: @merchant.id, name: 'Small Wooden Table')
+      @item2 = create(:item, merchant_id: @merchant.id, name: 'Practical Paper Keyboard')
+      @item3 = create(:item, merchant_id: @merchant.id, name: 'Gorgeous Bronze Bottle')
+      @item4 = create(:item, merchant_id: @merchant.id, name: 'Synergistic Linen Keyboard')
+      @item5 = create(:item, merchant_id: @merchant.id, name: 'Small Cotton Pants')
+
+      @invoice1 = create(:invoice, customer_id: @customer1.id)
+      @invoice2 = create(:invoice, customer_id: @customer1.id)
+      @invoice3 = create(:invoice, customer_id: @customer2.id)
+      @invoice4 = create(:invoice, customer_id: @customer2.id)
+      @invoice5 = create(:invoice, customer_id: @customer2.id)
+      @invoice6 = create(:invoice, customer_id: @customer3.id)
+      @invoice7 = create(:invoice, customer_id: @customer4.id)
+      @invoice8 = create(:invoice, customer_id: @customer5.id)
+
+      create(:transaction, invoice_id: @invoice1.id)
+      create(:transaction, invoice_id: @invoice2.id)
+      create(:transaction, invoice_id: @invoice3.id)
+      create(:transaction, invoice_id: @invoice4.id)
+      create(:transaction, invoice_id: @invoice5.id)
+      create(:transaction, invoice_id: @invoice6.id)
+      create(:transaction, invoice_id: @invoice7.id)
+      create(:transaction, invoice_id: @invoice8.id)
+
+      create(:invoice_item, invoice_id: @invoice1.id, item_id: @item1.id, status: 1, quantity: 3, unit_price: 500)
+      create(:invoice_item, invoice_id: @invoice2.id, item_id: @item3.id, quantity: 3, unit_price: 500)
+      create(:invoice_item, invoice_id: @invoice3.id, item_id: @item4.id, quantity: 3, unit_price: 500)
+      create(:invoice_item, invoice_id: @invoice4.id, item_id: @item1.id, quantity: 3, unit_price: 500)
+      create(:invoice_item, invoice_id: @invoice7.id, item_id: @item2.id, status: 1, quantity: 3, unit_price: 500)
+      create(:invoice_item, invoice_id: @invoice8.id, item_id: @item3.id, status: 2, quantity: 3, unit_price: 500)
+      create(:invoice_item, invoice_id: @invoice6.id, item_id: @item5.id, quantity: 3, unit_price: 500)
+      create(:invoice_item, invoice_id: @invoice5.id, item_id: @item4.id, quantity: 3, unit_price: 500)
+    end
+
+    it 'should see the invoice information id, status, created_at as Date, cutomer first and last name' do
+      visit "/merchant/#{@merchant.id}/invoices/#{@invoice1.id}"
+
+      within '.invoice' do
+        expect(page).to have_content("ID: #{@invoice1.id}")
+        expect(page).to have_content("Status: #{@invoice1.status}")
+        expect(page).to have_content("Date: #{@invoice1.created_at.strftime('%A %B %d, %Y')}")
+        expect(page).to have_content("Customer: #{@customer1.first_name} #{@customer1.last_name}")
+      end
+    end
+  end
 end
