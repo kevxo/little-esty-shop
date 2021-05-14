b87977baef1070e801f698d5a78a060027670357
Kevin Cuadros <kev040798@gmail.com>
2021-05-14 15:22:50 -0400

    Test clicking merchant name and being directed to merchant show

--- spec/features/admin/merchant_spec.rb
+++ spec/features/admin/merchant_spec.rb
@@ -23,5 +23,19 @@ RSpec.describe 'As a admin' do
         expect(page).to have_content(@merchant5.name)
       end
     end
+
+    it 'should click on name and be directed to the merchant show page' do
+      visit '/admin/merchants'
+
+      within '.all-merchants' do
+        expect(page).to have_link(@merchant1.name)
+
+        click_link @merchant1.name
+      end
+
+      expect(current_path).to eq("/admin/merchants/#{@merchant1.id}")
+      expect(page).to have_content(@merchant1.name)
+      expect(page).to_not have_content(@merchant2.name)
+    end
   end
 end
