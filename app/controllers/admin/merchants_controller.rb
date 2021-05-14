dace513bfa76abc6c7a70bd297e93dddc4ca9663
Kevin Cuadros <kev040798@gmail.com>
2021-05-14 16:56:28 -0400

    Create edit and update action for admin merchants controller

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,4 +6,23 @@ class Admin::MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    merchant.update(merchant_params)
+    merchant.save
+
+    flash[:notice] = 'Information has been successfully updated!'
+    redirect_to "/admin/merchants/#{merchant.id}"
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:name)
+  end
 end
