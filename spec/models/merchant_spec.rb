d5ea504b0494571225e49a92357e9bde62f53efb
Kevin Cuadros <kev040798@gmail.com>
2021-05-18 17:20:53 -0400

    Test for enum Enable and Disable

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many(:items) }
   end
+
+  describe 'validations' do
+    it { should define_enum_for(:status).with(%i[Enable Disable])}
+  end
 end
