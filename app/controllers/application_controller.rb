6276bc5717d819fa4417eee6468902acd6be4362
Kevin Cuadros <kev040798@gmail.com>
2021-05-27 22:44:16 -0400

    Add repo name and contributors names from github api to apllication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_action :repo_name, :collaborators
+
+  def repo_name
+    repo = Faraday.get('https://api.github.com/repos/kevxo/little-esty-shop') do |req|
+      req.headers['Authorization'] = ENV['Auth']
+    end
+
+    @repo_name = JSON.parse(repo.body, symbolize_names: true)[:name]
+  end
+
+  def collaborators
+    repo = Faraday.get('https://api.github.com/repos/kevxo/little-esty-shop/contributors') do |req|
+      req.headers['Authorization'] = ENV['Auth']
+    end
+
+    data = JSON.parse(repo.body, symbolize_names: true)
+
+    @collaborators = data.map do |user|
+      user[:login]
+    end.join(', ')
+  end
 end
